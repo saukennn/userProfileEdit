325d27f64330d0fc869e54d5f5eaee4b612cd718
saukennn <carlos.campos.1352424@sga.pucminas.br>
2022-03-19 20:17:54 -0300

    userEditProfile

---
+++ salvar.js
@@ -0,0 +1,16 @@
+dados = {
+    "data": [
+        {
+            "id": 1,
+            "nome": "Nome Completo",
+            "date": null,
+            "email": "exemplo@exemplo.com",
+            "genero": "Masculino Feminino ou Outro",
+            "senhaUsuario": "senha123",
+            "pagamento": "Mastercard - 02020 ******** 7331",
+            "endereço": "123 rua tal bairro tal",
+            "CEP": "123456-78"
+        }
+    ]
+}
+
